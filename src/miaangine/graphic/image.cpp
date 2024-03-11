c4cab859e9e366c45586027cdd498719084ccd77
Miaan <nghiabn2005@gmail.com>
2024-03-11 13:45:32 +0700

    add image class

---
+++ src/miaangine/graphic/image.cpp
@@ -0,0 +1,6 @@
+#include "image.hpp"
+
+namespace mia
+{
+    
+}
---
+++ src/miaangine/graphic/image.hpp
@@ -0,0 +1,12 @@
+#ifndef _MIA_IMAGE_HPP
+#define _MIA_IMAGE_HPP
+
+namespace mia
+{
+    class Image
+    {
+        
+    };
+}
+
+#endif

350396525be5b5fed48725a6029edc79df6d838a
Miaan <nghiabn2005@gmail.com>
2024-03-14 17:25:16 +0700

    add singleton util

--- src/demo/flappy-bird/main.cpp
+++ src/demo/flappy-bird/main.cpp
@@ -2,13 +2,17 @@
 #include <cstdlib>
 #include <ctime>
 
+#ifndef SDL_MAIN_HANDLED
+#define SDL_MAIN_HANDLED
+#endif
+
 int main(int argc, char *argv[])
 {
     srand(std::time(0));
 
-    Game *game = new Game();
+    // Game *game = new Game();
 
-    game->Run();
+    // game->Run();
 
     return 0;
 }

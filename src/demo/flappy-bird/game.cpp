7cd3b31d00c6eb3f8aa9699421f5c7c81c15cd49
Miaan <nghiabn2005@gmail.com>
2024-03-19 01:23:53 +0700

    add background (not use yet)

--- src/demo/flappy-bird/game.cpp
+++ src/demo/flappy-bird/game.cpp
@@ -12,6 +12,9 @@ void Game::Run()
     
     SImage *sImage = new SImage();
 
+    // mia::Image *background = new mia::Image("background.png", 400, 600, 0, 0);
+    // mia::renderer.imageHandle.RegisterImage(background);
+
     PipeManager *pipeManager = new PipeManager();
     Bird *bird = new Bird();
     

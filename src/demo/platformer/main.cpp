260199c7acb8e0fcfa3ac6345f49ad0aa3add64c
Miaan <nghiabn2005@gmail.com>
2024-03-20 14:20:55 +0700

    update demo

--- src/demo/platformer/main.cpp
+++ src/demo/platformer/main.cpp
@@ -2,19 +2,27 @@
 
 #include "box.hpp"
 
+#include <cstdlib>
+#include <ctime>
+
 #ifndef SDL_MAIN_HANDLED
 #define SDL_MAIN_HANDLED
 #endif
 
 int main(int argc, char *argv[])
 {
-    mia::generic.Init(400, 600, 0);
+    srand(std::time(0));
 
-    Box *box = new Box(5, 5);
+    mia::generic.Init(400, 600, 0);
 
     while (true)
     {
         mia::generic.NewFrame();
+
+        if (mia::input.GetButtonDown("Jump"))
+        {
+            Box *box = new Box(rand() % static_cast<int>(mia::camera.size()), rand() % 2 + 5);
+        }
 
         if (mia::input.IsQuit()) break;
 

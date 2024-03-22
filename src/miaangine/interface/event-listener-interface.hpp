4370cc04bf7c1d3c845cdffcc69dda5f334b5adc
Miaan <nghiabn2005@gmail.com>
2024-03-22 12:12:51 +0700

    Move EventListener interface

--- src/miaangine/interface/event-listener-interface.hpp
+++ src/miaangine/interface/event-listener-interface.hpp
@@ -1,14 +1,14 @@
 #ifndef _MIA_EVENT_LISTENER_INTERFACE_HPP
 #define _MIA_EVENT_LISTENER_INTERFACE_HPP
 
-#include <stdint.h>
-
 namespace mia 
 {
     class IEventListener
     {
     public:
-        virtual void Update(uint8_t message) = 0;
+        virtual ~IEventListener() = 0;
+ 
+        virtual void Update(unsigned char message) = 0;
     };
 }
 

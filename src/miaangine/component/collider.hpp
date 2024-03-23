997dd0cf2f0e01ee32f711ef14648cc88da734f3
Miaan <nghiabn2005@gmail.com>
2024-03-23 21:20:14 +0700

    Create Collider

---
+++ src/miaangine/component/collider.hpp
@@ -0,0 +1,43 @@
+#ifndef _MIA_COLLIDER_HPP
+#define _MIA_COLLIDER_HPP
+
+#include "object/object.hpp"
+
+#include "util/math.hpp"
+
+namespace mia
+{
+    class Collider
+    {
+    private:
+        friend class Object;
+        
+        Collider(Object *master, Vector2<> scale, Vector2<> offset);
+
+        virtual ~Collider();
+
+    private:
+        // Attributes
+        bool _isActive;
+        Object *_master;
+
+        Vector2<> _size;
+        Vector2<> _offset;
+    
+    public:
+        // Attributes accessing
+        bool isActive() const;
+        const Object& master() const;
+        const Vector2<>& size() const;
+        const Vector2<>& offset() const;
+
+        bool SetActive(bool newState);
+        Object& master();
+        Vector2<>& size();
+        Vector2<>& offset();
+
+        // Methods
+    };
+}
+
+#endif

090a6b678f1ea333edb5b4e05562a0b7342aa386
Miaan <nghiabn2005@gmail.com>
2024-03-26 09:31:42 +0700

    Update Tilemap abstraction

--- src/miaangine/graphic/tilemap-renderer.hpp
+++ src/miaangine/graphic/tilemap-renderer.hpp
@@ -1,10 +1,35 @@
 #ifndef _MIA_TILEMAP_RENDERER_HPP
 #define _MIA_TILEMAP_RENDERER_HPP
 
+#include "util/singleton.hpp"
+
+#include <vector>
+#include "util/math.hpp"
+#include "tilemap/tilemap-data.hpp"
+
 namespace mia
 {
-    class TilemapRenderer
+    struct TilemapHandle
     {
+        Vector<int> position;
+        TilemapData data;
+        bool isRender = false;
+    };
+
+    class TilemapRenderer : public Singleton<TilemapRenderer>
+    {
+    private:
+        friend class Singleton<TilemapRenderer>;
+        TilemapRenderer();
+        ~TilemapRenderer();
+
+    private:
+        std::vector<TilemapHandle*> _tilemaps;
+
+    public:
+        int AddTilemap(TilemapData& data);
+        bool LoadTilemap(int index);
+
     
     };
 }

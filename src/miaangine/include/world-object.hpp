3facbb633ce563f7739398b28f9f6b8ef2c86d1d
Miaan <nghiabn2005@gmail.com>
2024-03-30 00:33:40 +0700

    Basic abstraction of WorldObject

--- src/miaangine/include/world-object.hpp
+++ src/miaangine/include/world-object.hpp
@@ -16,9 +16,15 @@ namespace mia
         float _scale;
 
     public:
-        const mia::v2f localPosition() const;
-        const mia::v2f globalPosition() const;
-        const float 
+        mia::v2f localPosition() const;
+        mia::v2f globalPosition() const;
+        float scale() const;
+
+        mia::v2f& localPosition();
+        mia::v2f& globalPosition();
+        float& scale();
+
+        mia::v2f GetScreenPosition();
     };
 }
 

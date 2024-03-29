06f136f38f848117b122fa0e82fb50798665993e
Miaan <nghiabn2005@gmail.com>
2024-03-30 00:23:10 +0700

    Basic abstraction of Object

---
+++ src/miaangine/include/object.hpp
@@ -0,0 +1,39 @@
+#ifndef _MIA_OBJECT_HPP
+#define _MIA_OBJECT_HPP
+
+#include "entity.hpp"
+
+namespace mia
+{
+    class Object : public Entity
+    {
+    public:
+        Object(); //TODO add constructor
+        ~Object();
+
+    private:
+        mia::string _name;
+        bool active;
+        uint32_t tag;
+        Object *parent;
+        int scene;
+
+    public:
+        const mia::string& name() const;
+        bool isActive() const;
+        uint32_t getTag() const;
+        const Object* parent() const;
+        int getScene() const;
+
+        mia::string& name();
+        Object* parent();
+
+        bool SetActive(bool newState);
+        bool IsContainTag(int tag);
+        uint32_t AddTag(int tag);
+        uint32_t RemoveTag(int tag);
+        
+    };
+}
+
+#endif

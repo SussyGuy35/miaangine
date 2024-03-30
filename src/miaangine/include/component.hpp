a11158db2aadd9f4caaf1b74dfe83939fe7b1afd
Miaan <nghiabn2005@gmail.com>
2024-03-30 10:09:04 +0700

    Basic abstraction of Component

--- src/miaangine/include/component.hpp
+++ src/miaangine/include/component.hpp
@@ -1,11 +1,38 @@
 #ifndef _MIA_COMPONENT_HPP
 #define _MIA_COMPONENT_HPP
 
+#include "object.hpp"
+
 namespace mia
 {
-    class Component
+    class mia::Component : public Entity
     {
-        
+    public:
+    #pragma region Contructor Destructor
+        Component();
+        ~Component();
+    #pragma endregion
+
+    protected:
+    #pragma region Attribute
+        bool _active;
+        Object *_master;
+    #pragma endregion
+
+    public:
+    #pragma region Get-method
+        bool isActive() const;
+        const Object* master() const;
+    #pragma endregion
+
+    #pragma region Set-method
+        bool SetActive();
+        Object* SetMaster();
+    #pragma endregion
+
+    #pragma region Inherited method
+        const char* ToStr() const override;
+    #pragma endregion
     };
 }
 

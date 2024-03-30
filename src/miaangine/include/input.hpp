6a1dc7e5c91bdf059ce06c5b3a3d97946a4760f1
Miaan <nghiabn2005@gmail.com>
2024-03-30 16:06:01 +0700

    Update input decle

--- src/miaangine/include/input.hpp
+++ src/miaangine/include/input.hpp
@@ -8,13 +8,31 @@ namespace mia
     class Input : public Singleton<Input>
     {
     #pragma region Constructor Destructor
+    private:
         friend class Singleton<Input>;
         Input();
         ~Input();
     #pragma endregion
 
     #pragma region Attribute
-        
+    private:
+        bool _keysPressState[SDL_NUM_SCANCODES];
+        bool _keysDownState[SDL_NUM_SCANCODES];
+        bool _keysUpState[SDL_NUM_SCANCODES];
+
+        bool _isQuit;
+    #pragma endregion
+
+    #pragma region Public method
+        void Update();
+
+        bool GetKey(int key) const;
+        bool GetKeyDown(int key) const;
+        bool GetKeyUp(int key) const;
+    #pragma endregion
+
+    #pragma region Private method
+        void UpdateKeyInputEvent();
     #pragma endregion
     };
 }

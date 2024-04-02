fda3b1d61d22e0f289b783abb636ba86d6fd72d2
Miaan <nghiabn2005@gmail.com>
2024-04-02 19:41:10 +0700

    Basic body abstraction

---
+++ src/miaangine/include/body.hpp
@@ -0,0 +1,39 @@
+#ifndef _MIA_BODY_HPP
+#define _MIA_BODY_HPP
+#include "component.hpp"
+
+#include "sprite.hpp"
+
+namespace mia
+{
+    class Body : public Component
+    {
+    public:
+        Body(float mass, v2f initForce = v2f::zero());
+        virtual ~Body();
+
+    private:
+        float _mass;
+        v2f _velocity;
+        v2f _force;
+
+    public:
+        float mass() const;
+        v2f velocity() const;
+
+        float& mass();
+        v2f& velocity();
+
+        v2f AddForce(v2f force);
+        v2f AddForce(float x, float y);
+        v2f AddAcceleration(v2f acceleration);
+        v2f AddAcceleration(float x, float y);
+
+    public:
+        int getID() const override;
+        const char* ToStr() const override;
+        bool Init() override;
+    };
+}
+
+#endif
